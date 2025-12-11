ec7d3e5e4868600c3bf9f3da7f8ba43113e1a738
iphearum <phearumgic@gmail.com>
2025-12-11 17:07:16 +0700

    upgrade sso-client for next.js and nuxt.js

--- dist/env.js
+++ dist/env.js
@@ -1,11 +1,6 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.readEnvNumber = exports.readEnvString = void 0;
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
 const hasProcessEnv = typeof process !== 'undefined' && !!process.env;
 const readEnv = (key) => {
     if (!hasProcessEnv)
